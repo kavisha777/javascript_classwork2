a478269fcc63c0c6a423c49ad585c70ded80f5e8
kavisha777 <kaviyohen@gmail.com>
2025-03-12 19:54:07 +0530

    firstcommit

---
+++ COLORIFY/index.js
@@ -0,0 +1,14 @@
+function red(){
+    document.getElementById('circle').style.backgroundColor ="red";
+}
+
+
+function green(){
+    document.getElementById('circle').style.backgroundColor ="green";
+}
+
+
+
+function yellow(){
+    document.getElementById('circle').style.backgroundColor ="yellow";
+}
